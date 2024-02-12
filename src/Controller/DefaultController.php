d12e26707f35eefcc040b95d227cf4ffdf76d950
Mathis Tallaron <mathis.tallaron@gmail.com>
2024-02-12 11:47:34 +0100

    remplacement des noms dans le DefaultController (j'ai juste mis des noms "admin")

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'controller_name' => 'Admin',
         ]);
     }
 }
